24ecd1e7185d59df02cfed294c422936a58c81c0
MMM-tek <mtmirallesflores@gmail.com>
2026-03-28 12:37:06 +0000

    test blocks

---
+++ blocks.ts
@@ -0,0 +1,25 @@
+/**
+ * Minecraft Bedrock Blocks
+ */
+namespace blocks {
+    /**
+     * Spawns a mob at the given position
+     * @param mob name of the mob, eg: "zombie"
+     * @param x coordinate x
+     */
+    //% block="spawn %mob at x: %x"
+    //% color="#5B67A5"
+    export function spawnMob(mob: string, x: number): void {
+        // Logic goes here
+    }
+
+    /**
+     * Sets the world weather
+     * @param type weather type, eg: "clear"
+     */
+    //% block="set weather to %type"
+    //% color="#2D7EB8"
+    export function setWeather(type: string): void {
+        // Logic goes here
+    }
+}
